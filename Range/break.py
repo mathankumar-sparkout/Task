59fd2ef07044178ab1df92e91eac0ffa1da2872a
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-04-02 17:08:42 +0530

    4 files add

---
+++ Range/break.py
@@ -0,0 +1,13 @@
+for i in range(1,11):
+    #print(i)
+    if(i==5):
+        break    # only excute 4 value 5==5 break excute out of loop
+    print("bye",i)
+
+
+for word in "hello word":
+    if(word =='w'):
+        break     # only excute hello words
+    print(word) 
+
+
