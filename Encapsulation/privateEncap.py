ee402688b7bbde1e0887646b4d20f617b58ab470
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-04-02 13:22:33 +0530

    add encapsulation file

---
+++ Encapsulation/privateEncap.py
@@ -0,0 +1,9 @@
+class company():
+    def __init__(self):
+        self.__companyname="google"
+    def fun(self):
+        print(self.__companyname)
+obj=company()
+obj.fun()
+print(obj.__companyname)
+
