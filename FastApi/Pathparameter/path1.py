a3211bf8f1b1b8a9c353c6f9ede2cc3889a48470
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-04-19 23:35:43 +0530

    changes

--- FastApi/Pathparameter/path1.py
+++ FastApi/Pathparameter/path1.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from enum import Enum   #specfic type choose
 app=FastAPI()
 
@@ -10,20 +10,20 @@ class user(str, Enum): # class
 
 
 @app.get("/food/{food_name}")
-async def fun(food_name :user):
+async def fun(food_name :str):
     if food_name == user.vegetables:  #-> user type ==user class same 
-        return {"you are choose vegatables":food_name} #-> return user class value
+        return {"you are choose vegetables":food_name} #-> return user class value
     #elif(food_name ==user).fruits:
        # return {"you are choose fruits":food_name}
-    if food_name.value == user.fruits:               #-> same thing
+    elif food_name == user.fruits:               #-> same thing
         return {"your are choose fruits":food_name}
-    else:
+    elif  food_name == user.dairy:
         return {"you are choose dairy":food_name}
+    elif food_name != user:
+        raise HTTPException(status_code=404)
 
 
 
 
-#doubts:
-# elif->.value
-#13.data type
+
 
