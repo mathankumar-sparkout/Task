8edb1be3a5142c1c840c50ca50a9bb415ef759fc
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-04-20 23:20:11 +0530

    add file del.py

---
+++ Review2/del.py
@@ -0,0 +1,23 @@
+#---remove
+a=[1,2,3,4,5,6]
+a.remove(2) #-> remove 2 value
+print(a)
+
+#---clear
+
+a=[1,2,3,4,5,6]
+a.clear()    #-> clear all value
+print(a)
+
+
+#---pop
+a=[1,2,3,4,5,6]
+a.pop() #-> clear last element
+print(a)
+
+
+#--del 
+
+a=[1,2,3,4,5,6]
+del a[1]#-> remove index value
+print(a)
