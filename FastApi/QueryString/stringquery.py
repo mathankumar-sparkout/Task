eb08776c6faf5c8acbad4540c0fdddd2abc12594
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-04-22 18:04:42 +0530

    add file

---
+++ FastApi/QueryString/stringquery.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI,Query
+app=FastAPI()
+
+@app.get('/root')
+async def fun(q: list[str]| None =Query(default ="abc",max_length=3,description="hii",deprecated=True,include_in_schema=False)):#->default take value#->default=... its is a requird..#include_in_schema is delete input filed 
+    result={"value":[{"id":"foo"},{"id":"barr"}]}
+    if q:
+        result.update({"q":q})
+    return result
+
