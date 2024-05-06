d84043df2a24ec95fd1469fbb0f71257b082a660
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-05-06 15:50:22 +0530

    add files

--- FastApi/BackgroundTasks/background.py
+++ FastApi/BackgroundTasks/background.py
@@ -1,14 +1,19 @@
 
 from fastapi import FastAPI,BackgroundTasks # import background task
- 
+import time
 app=FastAPI()
 
 def fun(email:str,message=""): 
     with open("log.txt",mode="w") as email_file: # file open and write a file
-        content=f"notification{email}:{message}" 
+        
+        content=f"notification{email}:{message}"
+        time.sleep(5) 
         email_file.write(content)
         
 @app.post("/notification/{emil}") # path parameter
 async def notifi(email :str,background:BackgroundTasks): # background query parameter
     background.add_task(fun,email,message="some notification") # add the function (message:return log.txt)
     return{"message":"Notification send in the background"} # return msg
+
+
+
