0cf1e118cca7b8eafa584667db8fbe623bd79da5
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-05-06 14:43:08 +0530

    add file

--- FastApi/MetadataAndDocs/metadata.py
+++ FastApi/MetadataAndDocs/metadata.py
@@ -1,20 +1,24 @@
-from fastapi import FastAPI
-
-app=FastAPI(title="Python",
-            description="description",
-            summary="hello word",
-            version=3.0,
-            contact={
-                "name":"fastapi",
-                "url":"https:www.fastapi.com",
-                "email":"fastapi@123.com",
-            })
-@app.get("/item")
-async def fun():
-    return [{"name":"java"}]
+from fastapi import FastAPI  # import fastapi
 
 
+tags_metadata = [  # get/post/put/delete  box
+    {
+        "name": "fastapi",
+        "description": "hii python",
+    },
+    {"name": "python", "description": "fastapi to sql server"},
+]
 
 
+app = FastAPI(
+    title="python Api",  # logo
+    description="FastAPI easy to learn  🚀",  # after summary line
+    summary="FastApi is good app",  # top of the page
+    terms_of_service="http://example.com/terms/",  # link
+    openapi_tags=tags_metadata,
+)
 
-    
+
+@app.get("/user", tags=["User"])
+async def meta():
+    return {"name": "python"}
