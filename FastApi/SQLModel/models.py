87a456aa7bfab68c01daa879d4103fc7522425c5
mathankumar-sparkout <mathankumar@sparkouttech.com>
2024-05-12 15:38:44 +0530

    add file

--- FastApi/SQLModel/models.py
+++ FastApi/SQLModel/models.py
@@ -1,10 +1,17 @@
 from sqlmodel import SQLModel,Field
 from typing import Optional
+#from pydantic import BaseModel
 
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
     id: Optional[int] = Field(None, primary_key=True)
     name: str = None
-    email: int
+    email: str = None
     password: str
+    
+#class res_User(BaseModel):
+   ## name: str = None
+    #email: int
+    #password: str
+    
--- FastApi/SQLModel/routers/user.py
+++ FastApi/SQLModel/routers/user.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 import database, models
-from typing import List
+from typing import List,Union
 from sqlmodel import Session,select
-#from database import SessionLocal
 from models import User
-#from main import session
+from database import engine
 
 
 router = APIRouter(tags=["User"])
@@ -14,28 +13,66 @@ get_db = database.get_db
 
 
 
-@router.post("/post_user")
+
+
+#get all user data----
+
+@router.get("/get_all_user",response_model=List[models.User],status_code=status.HTTP_200_OK)
+def get_user(db: Session = Depends(get_db)):
+        stmt=select(models.User)
+        result=db.exec(stmt).all()
+        return result
+    
+    
+# get single user data------
+
+@router.get("/get_user/{id}",response_model=Union[models.User,str])
+def get_single_user(id :int,response:Response,db: Session = Depends(get_db)):
+        user=db.get(models.User, id)
+        if user is None:
+            response.status_code=404
+            return {f"none{id}"}
+        #result=jsonable_encoder(models.User)
+        return user
+    
+
+@router.post("/post_user",response_model=models.User,status_code=201)
 # only we want name and password use this reponse_model
 def create_user(request: models.User, db: Session = Depends(get_db)):
-    new_user = models.User(
-        name=request.name,
-        email=request.email,
-        password=request.password
-    )
-    db.add(new_user) 
+    db.add(request) 
     db.commit() 
-    db.refresh(new_user)
-    return new_user
+    db.refresh(request)
+    return request
+
+@router.put("/update_user/{id}",response_model=Union[models.User,str])
+def update_user(id:int,update_user:models.User,response:Response,db:Session=Depends(get_db)):
+    user=db.get(models.User,id)
+  
+    
+    if user is None:
+        response.status_code=404
+        return "User not found"
+    #user_dict= {"name": update_user.name, "email": update_user.email, "password": update_user.password}
+    user_dict=update_user.dict(exclude_unset=True)
+    for key,val in user_dict.items():
+        setattr(user,key,val)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
 
 
-@router.get("/get_user",response_model=List[models.User],status_code=status.HTTP_200_OK)
-def get_user():
-    statement=select(models.User)
-    result=Session.exec(statement).all()
-    return result
-    #blogs = Session.exec(models.User).all()
-    #return blogs
-
-
+@router.delete("/delete user/{id}")
+def delete_user(id:int,response:Response,db:Session=Depends(get_db)):
+    user=db.get(models.User,id)
+    if user is None:
+        response.status_code=404
+        return {"not found"}
+    db.delete(user)
+    db.commit()
+    return Response(status_code=200)
+    
+    
+    
 
 
